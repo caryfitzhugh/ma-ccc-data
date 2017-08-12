9e73eb15d5b5046032099c68ee6928f020e2cba1
Cary FitzHugh <cary.fitzhugh@gmail.com>
2017-08-11 21:04:43 -0400

    poking around

--- src/ma_ccc_data/core.clj
+++ src/ma_ccc_data/core.clj
@@ -1,6 +1,21 @@
-(ns ma-ccc-data.core)
+(ns ma-ccc-data.core
+  (:import
+   (java.io FileInputStream FileNotFoundException IOException File)
+   (java.util.zip ZipInputStream ZipEntry)))
 
+(defn entries [zipfile]
+  (lazy-seq
+   (if-let [entry (.getNextEntry zipfile)]
+     (cons entry (entries zipfile)))))
+
+(defn walkzip [fileName]
+  (with-open [z (ZipInputStream. (FileInputStream. fileName))]
+    (doall (map #(.toString %) (entries z)))))
+
+(comment
+  (println (walkzip "MAEEA_Data/TG/TG.obs.zip")))
 (defn -main
   "I don't do a whole lot."
-  [& args ]
-  (println ))
+  [& args]
+  (doall (walkzip "MAEEA_Data/TG/TG.obs.zip"))
+  (println))
