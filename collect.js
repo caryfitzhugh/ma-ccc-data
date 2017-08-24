cd8bdc76460dc5af7c9b6f4b8156a1f02a03bb67
Cary FitzHugh <cary.fitzhugh@gmail.com>
2017-08-24 16:11:02 -0400

    pulling out the other metrics

--- collect.js
+++ collect.js
@@ -38,6 +38,68 @@ const file_map = {
     }
   };
 
+const datagrapher_seasons = {
+  "annual": ["ANN", ""],
+  "spring": ["MAM", "-05"],
+  "summer": ["JJA", "-08"],
+  "fall"  : ["SON", "-11"],
+  "winter": ["DJF", "-02"]
+};
+const datagrapher_location_map = {
+  "MA": "MA",
+  "Essex": "25009",
+  "Franklin": "25011",
+  "Worcester": "25027",
+  "Dukes": "25007",
+  "Plymouth": "25023",
+  "Hampshire": "25015",
+  "Bristol": "25005",
+  "Hampden": "25013",
+  "Berkshire": "25003",
+  "Middlesex": "25017",
+  "Suffolk": "25025",
+  "Barnstable": "25001",
+  "Norfolk": "25021",
+  "Nantucket": "25019"
+};
+const datagrapher_files = () => {
+  let all_files = [];
+  let seasons = [ ["ANN", 'annual'],
+                  ['MAM', 'spring'],
+                  ['JJA', 'summer'],
+                  ['SON', 'fall'],
+                  ['DJF', 'winter'] ];
+  let metrics = [ ['maxt', 'temperature', 'tmax'],
+                  ['mint', 'temperature', 'tmin'],
+                  ['avgt', 'temperature', 'tavg'],
+                  ['pcpn', 'precipitation', 'tot'],
+                  ['gdd50', 'degree_days', 'g'],
+                  ['hdd50', 'degree_days', 'h'],
+                  ['cdd50', 'degree_days', 'c'],
+                  ['tx90', 'above_temp_thresholds', 'gt90'],
+                  ['tx95', 'above_temp_thresholds', 'gt95'],
+                  ['tx100', 'above_temp_thresholds', 'gt100'],
+                  ['tn0', 'below_temp_thresholds', 'lt0'],
+                  ['tn32', 'below_temp_thresholds', 'lt32'],
+                  ['pcpn_1', 'precip_events', 'gt1in'],
+                  ['pcpn_2', 'precip_events', 'gt2in'],
+                  ['pcpn_4', 'precip_events', 'gt4in']
+                ];
+  metrics.forEach((metric) => {
+    seasons.forEach((season) => {
+      metrics.forEach((metric) => {
+        all_files.push([`observed_state_${metric[0]}_${season[0]}`,
+                          metric[1], ['obs'], season[1], metric[2]]);
+
+        all_files.push([`projected_state_${metric[0]}_${season[0]}`,
+                          metric[1], ['low','med','high'], season[1], metric[2]]);
+      });
+    });
+  });
+
+  return all_files;
+};
+
 const strip_years = (file, step) => {
   //let attr_path = [root, year, scenario, season, metric];
   let years = _.uniq(_.flatten(
@@ -119,10 +181,48 @@ const collect = (outfile, mappings) => {
   });
 
   let output_filename = path.join(output_path, outfile +".json")
-  result = strip_years(result, 5);
-  fs.writeFileSync(output_filename, JSON.stringify(result, null, 2));
+  stripped_result = strip_years(result, 5);
+  fs.writeFileSync(output_filename, JSON.stringify(stripped_result, null, 2));
+  return result;
 };
 
-Object.keys(file_map).forEach((filename) => {
-  collect(filename, file_map[filename]);
+let all_data = Object.keys(file_map).reduce((memo, filename) => {
+  memo[filename] = collect(filename, file_map[filename]);
+  return memo;
+}, {});
+
+// Now do conversion for Datagraher
+datagrapher_files().forEach((file_spec) => {
+  let filename = file_spec[0];
+  let data_sel = file_spec[1];
+  let source_sel = file_spec[2];
+  let season_sel = file_spec[3];
+  let season_spec = datagrapher_seasons[season_sel];
+  let metric_sel = file_spec[3];
+  let year_results = {};
+
+  let data = all_data[data_sel];
+
+  // Location
+  Object.keys(data).forEach((location) => {
+    let loc_data = data[location];
+    Object.keys(loc_data).forEach((year) => {
+      let metric_data = source_sel.map((sel) => {
+        let year_data = loc_data[year][source_sel];
+        if (year_data) {
+          let season_data = year_data[season_sel];
+          return season_data[metric_sel];
+        }
+      });
+      setPath(year_results, [year, datagrapher_location_map[location]], metric_data);
+    });
+  });
+
+  let result = Object.keys(year_results).map((year) => {
+    return [year, year_results[year]];
+  });
+
+  let output_filename = path.join(output_path, filename);
+  fs.writeFileSync(output_filename, JSON.stringify({data: result}, null, 2));
+  console.log("wrote:", output_filename);
 });
